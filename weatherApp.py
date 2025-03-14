cf1cd46549a6fbce5c0397c163d7a110bf4d0c2e
Syed-Hammad-Atif <hammadatif738@gmail.com>
2025-03-14 18:17:30 +0500

    Add files via upload

---
+++ weatherApp.py
@@ -0,0 +1,23 @@
+import requests
+
+api_key = 'adaf972289e58e9f8a65cc9e8f4568d5'
+
+print("Welcome to Weather App")
+
+while True:
+    user_input = input("Enter City: ")
+    weather_data = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={user_input}&appid={api_key}&units=metric")
+
+    if weather_data.json()['cod'] == '404':
+        print("No City found")
+    else:
+        weather = weather_data.json()['weather'][0]['main']
+        temp = weather_data.json()['main']['temp']
+
+        print(f"The weather in {user_input} is: {weather}")
+        print(f"The temperature in {user_input} is: {temp}°C")
+    
+    choice = input("\nDo you want to check another city? (y/n): ").lower()
+    if choice != 'y':
+        print("Exiting the weather app")
+        break
